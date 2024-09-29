99595145dcee86a9f8eb42deb9de3d72cbd2afb9
thevaultboy11 <thevaultboy11@gmail.com>
2024-09-29 16:36:40 +0200

    fix

--- landing-page/src/navigation.ts
+++ landing-page/src/navigation.ts
@@ -16,18 +16,6 @@ export const headerData = {
         {
           text: 'The Suppressed Education',
           href: getPermalink('/library/the-suppressed-education'),
-        },
-        {
-          text: 'Get Toned Abs',
-          href: getPermalink('/library/get-toned-abs'),
-        },
-        {
-          text: 'Lucky Girl Syndrome',
-          href: getPermalink('/library/lucky-girl-syndrome'),
-        },    
-        {
-          text: 'Remove Social Anxiety',
-          href: getPermalink('/library/remove-social-anxiety'),
         }
       ],
     },
@@ -41,10 +29,7 @@ export const footerData = {
       links: [
         { text: 'Magnetic Aura', href: '/library/magnetic-aura' },
         { text: 'Arcane Stance', href: '/library/mastering-fate' },
-        { text: 'The Suppressed Education', href: '/library/the-suppressed-education' },
-        { text: 'Get Toned Abs', href: '/library/bridging-afterlife' },
-        { text: 'Lucky Girl Syndrome', href: '/library/anunnaki-unveiled' },
-        { text: 'Remove Social Anxiety', href: '/library/anunnaki-unveiled' }
+        { text: 'The Suppressed Education', href: '/library/the-suppressed-education' }
       ],
     },
   ],
