12ff22c9e18132556d0a7d1d4dd6153acd59c7f1
thevaultboy11 <thevaultboy11@gmail.com>
2024-10-14 14:20:12 +0200

    update

--- landing-page/src/navigation.ts
+++ landing-page/src/navigation.ts
@@ -27,9 +27,14 @@ export const footerData = {
     {
       title: 'Our Library',
       links: [
-        { text: 'Magnetic Aura', href: '/library/magnetic-aura' },
-        { text: 'Arcane Stance', href: '/library/mastering-fate' },
-        { text: 'The Suppressed Education', href: '/library/the-suppressed-education' }
+        { text: 'Magnetic Aura', href: 'https://takeshim.gumroad.com/l/magneticauraebook' },
+        { text: 'Arcane Stance', href: 'https://takeshim.gumroad.com/l/arcanestance' },
+        { text: 'The Suppressed Education', href: 'https://takeshim.gumroad.com/l/thesuppressededucation' },
+        { text: 'Book of Wisdom', href: 'https://revivalofwisdom.com/products/book-of-wisdom-pdf-english?sca_ref=7186286.vYxyAp6dwA' },
+        { text: 'Alkaline Awakening', href: 'https://revivalofwisdom.com/products/alkaline-awakening-book?sca_ref=7186286.vYxyAp6dwA' },
+        { text: 'The Wisdom Bundle', href: 'https://revivalofwisdom.com/products/the-wisdom-bundle?sca_ref=7186286.vYxyAp6dwA' },
+        { text: 'Vibrational Energy Codes', href: 'https://revivalofwisdom.com/products/vibrational-energy-codes-pdf?sca_ref=7186286.vYxyAp6dwA' },
+        { text: 'Human Health Guide', href: 'https://revivalofwisdom.com/products/human-health-guide-pdf?sca_ref=7186286.vYxyAp6dwA' },
       ],
     },
   ],
