4c4f68043fbdbde53eb01cb7a003fabaa9105a2e
thevaultboy11 <thevaultboy11@gmail.com>
2024-10-14 14:22:10 +0200

    fix

--- landing-page/src/navigation.ts
+++ landing-page/src/navigation.ts
@@ -1,26 +1,6 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
-export const headerData = {
-  links: [
-    {
-      text: 'Our Library',
-      links: [
-        {
-          text: 'Magnetic Aura',
-          href: getPermalink('/library/magnetic-aura'),
-        },
-        {
-          text: 'Arcane Stance',
-          href: getPermalink('/library/arcane-stance'),
-        },
-        {
-          text: 'The Suppressed Education',
-          href: getPermalink('/library/the-suppressed-education'),
-        }
-      ],
-    },
-  ],
-};
+
 
 export const footerData = {
   links: [
